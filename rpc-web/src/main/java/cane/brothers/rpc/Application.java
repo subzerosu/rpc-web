b05a1686d115c580bf2698a7ae0095f2ed40747f
cane <webcane@ya.ru>
2016-04-05 00:00:53 +0300

    init

---
+++ rpc-web/src/main/java/cane/brothers/rpc/Application.java
@@ -0,0 +1,13 @@
+package cane.brothers.rpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan(basePackageClasses = {GoogleAutoConfiguration.class})
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
