e952518141cffee0839459486d35e0198c6371eb
mniedre <webcane@ya.ru>
2017-02-12 19:44:08 +0300

    add java doc

--- rpc-web/src/main/java/cane/brothers/rpc/service/GoogleConnection.java
+++ rpc-web/src/main/java/cane/brothers/rpc/service/GoogleConnection.java
@@ -2,7 +2,15 @@ package cane.brothers.rpc.service;
 
 import com.google.api.services.sheets.v4.Sheets;
 
+/**
+ * Класс для связи с google сервисами.
+ *
+ * @author cane
+ */
 public interface GoogleConnection {
-	
+
+	/**
+	 * @return Сервис google таблиц.
+	 */
 	Sheets getSheetsService();
 }
