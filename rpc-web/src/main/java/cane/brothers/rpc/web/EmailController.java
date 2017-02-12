d362311cfadc12196e0bc50de48165600c6752e5
mniedre <webcane@ya.ru>
2017-02-12 16:28:17 +0300

    add draft controllers

---
+++ rpc-web/src/main/java/cane/brothers/rpc/web/EmailController.java
@@ -0,0 +1,34 @@
+package cane.brothers.rpc.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cane.brothers.rpc.service.email.EmailManager;
+
+@RestController
+@RequestMapping(value = "/api")
+public class EmailController extends BaseController {
+	
+	protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	private EmailManager email;
+
+	@GetMapping(value = "/email")
+	public ResponseEntity<List<String>> sendEmail() {
+		email.sendEmail("hello");
+		
+		List<String> responseBody = new ArrayList<String>();
+		responseBody.add("Ok");
+		return new ResponseEntity<List<String>>(responseBody, HttpStatus.OK);
+	}
+}
---
+++ rpc-web/src/main/java/cane/brothers/rpc/web/RpcController.java
@@ -0,0 +1,39 @@
+package cane.brothers.rpc.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cane.brothers.rpc.service.fc.Rpc;
+
+@RestController
+@RequestMapping(value = "/api")
+public class RpcController extends BaseController {
+
+	protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	private Rpc rpc;
+
+	@GetMapping(value = "/rpc")
+	public ResponseEntity<List<String>> sendEmail() {
+		String ticket = rpc.sendRequest();
+		List<String> responseBody = new ArrayList<String>();
+
+		if (ticket != null) {
+			responseBody.add("Ok");
+			return new ResponseEntity<List<String>>(responseBody, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<List<String>>(responseBody, HttpStatus.ACCEPTED);
+		}
+
+	}
+}
