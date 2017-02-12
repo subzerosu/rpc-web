1bba89345fdba624ed3dd49ba4e0d3fb1d8ff279
mniedre <webcane@ya.ru>
2017-02-12 19:47:19 +0300

    rename services

--- rpc-web/src/main/java/cane/brothers/rpc/service/email/EmailManager.java
+++
@@ -1,6 +0,0 @@
-package cane.brothers.rpc.service.email;
-
-public interface EmailManager {
-
-	boolean sendEmail(String output);
-}
--- rpc-web/src/main/java/cane/brothers/rpc/service/fc/Rpc.java
+++
@@ -1,6 +0,0 @@
-package cane.brothers.rpc.service.fc;
-
-public interface Rpc {
-
-	String sendRequest();
-}
