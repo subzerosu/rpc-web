dead30e18ba3a1ff76fb0d1d413e3784cc14192b
cane <webcane@ya.ru>
2017-02-26 20:58:06 +0300

    AngularJs CSRF

--- rpc-web/src/main/resources/static/app/app.js
+++ rpc-web/src/main/resources/static/app/app.js
@@ -1,5 +1,19 @@
 (function () {
-  'use strict';
-  
-  var app = angular.module('rpcApp', ['ngMaterial', 'ngMessages', 'ngResource', 'ngMdIcons']);
+    'use strict';
+
+    var app = angular.module('rpcApp', ['ngMaterial', 'ngMessages', 'ngMdIcons', 'ngRoute']);
+    app.config(['$httpProvider', '$locationProvider', '$logProvider',
+        function ($httpProvider, $locationProvider, $logProvider) {
+            // #
+            $locationProvider.hashPrefix('!');
+
+            //$locationProvider.html5Mode(true);
+
+            // angular csrf
+            $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+
+            // allow debug
+            //$logProvider.debugEnabled(true);
+        }]);
+
 })();
