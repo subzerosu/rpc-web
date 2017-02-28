990eb0930153e16de8afc3579f5bf8c285d27f19
mniedre <webcane@ya.ru>
2017-02-28 17:29:26 +0300

    nav-bar label

--- rpc-web/src/main/resources/static/app/pages/main.js
+++ rpc-web/src/main/resources/static/app/pages/main.js
@@ -1,28 +1,56 @@
-(function (angular) {
-    'use strict';
-    var ctrl_name = 'mainCtrl';
-    var url_pattern = '/';
+(function(angular) {
+	'use strict';
+	var ctrl_name = 'mainCtrl';
+	var url_pattern = '/';
 
-    angular.module('rpcApp')
-        .controller(ctrl_name, ["$scope", "$location", "$http", "$mdSidenav", "$log", MainController])
-    .config(['$routeProvider', MainConfig]);
+	angular.module('rpcApp').controller(
+			ctrl_name,
+			[ "$scope", "$location", "$http", "$mdSidenav", "$log",
+					MainController ]).config([ '$routeProvider', MainConfig ]);
 
-    function MainController($scope, $location, $http, $mdSidenav, $log) {
-        var self = this;
-        $scope.name = ctrl_name;
+	function MainController($scope, $location, $http, $mdSidenav, $log) {
+		var self = this;
+		$scope.name = ctrl_name;
 
-        $scope.currentNavItem = 'taskPage';
-    };
+		self.navItems = [{
+			label : 'Главная',
+			name : 'mainPage'
+		}, {
+			label : 'Задания',
+			name : 'taskPage'
+		}, {
+			label : 'Источник',
+			name : 'sourcePage'
+		}, {
+			label : 'Настройки',
+			name : 'configPage'
+		}];
 
+		self.currentNavItem = 'taskPage';
+		$scope.currentNavLabel = 'Задания';
+		
+		$scope.$watch('currentNavItem', function(it) {
+			$scope.currentNavLabel = self.getByName(it);
+	    });
+		
+		self.getByName = function(itemname) {
+			var itemName = itemname;
+			var positiveArr = self.navItems.filter(function(item) {
+				  return itemName == item.name;
+			});
+			if(Array.isArray(positiveArr) && positiveArr.length) {
+				return positiveArr[0].label;
+			}
+		};
+	};
 
-    function MainConfig($routeProvider) {
-        $routeProvider
-            .when(url_pattern, {
-                templateUrl: './app/pages/main.html',
-                controller: ctrl_name
-            }).otherwise({
-            templateUrl: './app/pages/main.html'
-        });
-    };
+	function MainConfig($routeProvider) {
+		$routeProvider.when(url_pattern, {
+			templateUrl : './app/pages/main.html',
+			controller : ctrl_name
+		}).otherwise({
+			templateUrl : './app/pages/main.html'
+		});
+	};
 
 })(angular);
