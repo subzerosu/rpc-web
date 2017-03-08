5c600029f184fb50272530a8eb7a941e24c092e8
cane <webcane@ya.ru>
2017-03-08 13:43:59 +0300

    add task component

--- rpc-web/src/main/resources/static/app/pages/task.js
+++ rpc-web/src/main/resources/static/app/pages/task.js
@@ -1,28 +1,11 @@
-(function (angular) {
-    'use strict';
-    var ctrl_name = 'taskCtrl';
-    var url_pattern = '/tasks';
+(function(angular) {
+	'use strict';
 
-    angular.module('rpcApp')
-        .controller(ctrl_name, ["$scope", "$log", "$routeParams", TaskController])
-        .config(['$routeProvider', TaskConfig]);
-
-    function TaskController($scope, $log, $routeParams) {
-        var self = this;
-
-        $scope.name = ctrl_name;
-        $scope.params = $routeParams;
-    };
-
-
-    function TaskConfig($routeProvider) {
-        $routeProvider
-            .when(url_pattern, {
-                templateUrl: './app/pages/task.html',
-                controller: ctrl_name
-            }).otherwise({
-            templateUrl: './app/pages/main.html'
-        });
-    };
+	angular.module('rpcApp').component('task', {
+        bindings: {
+            task: '<'
+        },
+		templateUrl : 'app/pages/task.html'
+	});
 
 })(angular);
