470892b89500bf317831b9971df57b595aa76c03
cane <webcane@ya.ru>
2017-04-03 00:22:47 +0300

    fix #18 login

--- rpc-web/src/main/resources/static/app/pages/dashboard.js
+++ rpc-web/src/main/resources/static/app/pages/dashboard.js
@@ -2,7 +2,43 @@
     'use strict';
 
     angular.module('rpcApp').component('dashboard', {
-        templateUrl: 'app/pages/dashboard.html'
+        templateUrl: 'app/pages/dashboard.html',
+        controller : DashboardController
     });
+
+    function DashboardController($rootScope, $http, $location) {
+        var self = this;
+
+        var authenticate = function(credentials, callback) {
+            var headers = credentials ? {
+                authorization : "Basic "
+                + btoa(credentials.username + ":"
+                    + credentials.password)
+            } : {};
+
+            $http.get('/api/rpc/user', {
+                headers : headers
+            }).then(function(response) {
+                if (response.data.name) {
+                    $rootScope.authenticated = true;
+                } else {
+                    $rootScope.authenticated = false;
+                }
+                callback && callback($rootScope.authenticated);
+            }, function() {
+                $rootScope.authenticated = false;
+                callback && callback(false);
+            });
+        }
+
+        authenticate();
+
+        self.logout = function() {
+            $http.post('logout', {}).finally(function() {
+                $rootScope.authenticated = false;
+                $location.path("/");
+            });
+        }
+    };
 
 })(angular);
